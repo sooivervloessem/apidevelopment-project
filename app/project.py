77bd5fce2fc730035af8b3fc546f514692f14431
sooivervloessem <r0834426@student.thomasmore.be>
2022-11-13 15:37:34 +0100

    added 3 get and 1 post

--- app/project.py
+++ app/project.py
@@ -1,35 +1,56 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from random import randint
 
 app = FastAPI()
 
+class KpopSong(BaseModel):
+    song: str
+    artist: str
+    album: str | None = None
+    release_date: str | None = None
+
+
 kpop_songs = {"0": {
-    "song": "Shut Down",
-    "artist": "Blackpink",
-    "album": "Born Pink",
-    "release_date": "2022"
-}}
+        "song": "Shut Down",
+        "artist": "Blackpink",
+        "album": "Born Pink",
+        "release_date": "2022"
+    },
+    "1": {
+        "song": "After Like",
+        "artist": "IVE",
+        "album": "After Like",
+        "release_date": "2022"
+    }
+}
+
 
 @app.get("/kpop")
 async def get_kpop_songs():
     return kpop_songs
 
-@app.get("/percentage/{lower_limit}/{upper_limit}")
-async def get_random_percentage1(lower_limit: int, upper_limit: int):
-    if(lower_limit >= upper_limit):
-        return {'error': 'The upper limit must be greater than the lower limit!', 'lower limit': lower_limit, 'upper limit': upper_limit}
-    return {'percentage': randint(lower_limit, upper_limit)}
 
-@app.get("/percentage/{lower_limit}/{upper_limit}/{amount}")
-async def get_random_percentage2(lower_limit: int, upper_limit: int, amount: int):
-    if(lower_limit >= upper_limit):
-        return {'error': 'The upper limit must be greater than the lower limit!', 'lower limit': lower_limit, 'upper limit': upper_limit}
+@app.get("/kpop/random")
+async def get_random_kpop_song():
+    lower_limit = 0
+    upper_limit = len(kpop_songs) - 1
+    if (lower_limit > upper_limit):
+        return {'error': 'The list of kpop songs may not be empty'}
+    return kpop_songs[str(randint(lower_limit, upper_limit))]
 
-    if (amount <= 0):
-        return {'error': 'The amount must be greater than 0!', 'amount': amount}
 
-    random_numbers = []
-    for counter in range(amount):
-        random_numbers.append(randint(lower_limit, upper_limit))
+@app.get("/kpop/{id}")
+async def get_kpop_song_by_id(id: int):
+    if id >= len(kpop_songs):
+        return {'error': 'There is no song matching that id.'}
+    if id < 0:
+        return {'error': 'Id cannot be less than 0.'}
+    return kpop_songs[str(id)]
 
-    return {'percentages': random_numbers}
+
+@app.post("/kpop", response_model=KpopSong)
+async def create_kpop_song(kpop_song: KpopSong):
+    kpop_songs[str(len(kpop_songs))] = kpop_song
+    return kpop_song
+
