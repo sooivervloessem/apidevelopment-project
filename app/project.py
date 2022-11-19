18dda2cb95e0215a6483b9e80935bfba9b241f25
sooivervloessem <r0834426@student.thomasmore.be>
2022-11-19 13:53:15 +0100

    second try fix front-end

--- app/project.py
+++ app/project.py
@@ -6,17 +6,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "https://localhost.tiangolo.com",
-    "http://127.0.0.1:5500",
     "https://sooivervloessem.github.io",
     "https://sooivervloessem.github.io/apidevelopment-project"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
